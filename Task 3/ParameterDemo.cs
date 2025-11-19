ac5b2604e35d8a162dbf6dc02fa76bf88764c275
Roshna-karki <xettrirsohna12@gmail.com>
2025-11-19 11:59:11 +0545

    Task 3

---
+++ Task 3/ParameterDemo.cs
@@ -0,0 +1,25 @@
+﻿public class ParameterDemo
+{
+    // 1. Increase using ref
+    public void Increase(ref int number)
+    {
+        number = number + 10;
+    }
+
+    // 2. GetFullName using out
+    public void GetFullName(out string fullname)
+    {
+        fullname = "Roshna Karki";
+    }
+
+    // 3. SumAll using params
+    public int SumAll(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        return sum;
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Create object of ParameterDemo
+        ParameterDemo obj = new ParameterDemo();
+
+        // Call Increase()
+        int value = 20;
+        obj.Increase(ref value);
+        Console.WriteLine("Value after Increase: " + value);
+
+        // Call GetFullName()
+        string name;
+        obj.GetFullName(out name);
+        Console.WriteLine("Full Name: " + name);
+
+        // Call SumAll()
+        int total = obj.SumAll(5, 10, 15, 20);
+        Console.WriteLine("Sum of all numbers: " + total);
+    }
+}
